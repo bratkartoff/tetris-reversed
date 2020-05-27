53fda61c8f086db398a9f9f9486916cd8c068000
Nils <NLoewen@Gauselmann.de>
2020-05-27 19:34:30 +0200

    Included all my chnages that last commit mist out on

--- config.py
+++ config.py
@@ -4,6 +4,9 @@ class Config:
     def __init__(self):
         self.font = pg.font.SysFont(None, 20)
         self.displaysize = (800, 600)
+        self.play_area = (600,300)
+        self.grid = (20,10)
+        self.box = 30
         self.fps = 5
 
 config = Config()
